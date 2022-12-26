c52f6003efeab2a9ce21fb569b49b6c2298fc7f0
kym <kym93358@gmail.com>
2022-12-26 20:22:59 +0900

    imu_to_odom_3

--- node/pure_pursite/imu_to_odom.py
+++ node/pure_pursite/imu_to_odom.py
@@ -85,6 +85,7 @@ if __name__ == '__main__':
         rospy.init_node('odom_node', anonymous=True)
         
         sub = rospy.Subscriber('/imu', Imu, imu_to_odom ,queue_size=10)
+        rospy.spin()
         
     except rospy.ROSInterruptException:
         pass
